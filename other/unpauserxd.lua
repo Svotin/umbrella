d2611ca2203eb26e3ab489c96058e5be61201096
Svotin <dogweb2023@gmail.ru>
2018-05-26 16:28:37 +0300

    addDelayBetweenExecution

--- other/unpauserxd.lua
+++ other/unpauserxd.lua
@@ -5,8 +5,13 @@ function unpauser.OnUpdate()
 	if not Menu.IsEnabled(unpauser.optionEnable) or not Engine.IsInGame() or not Heroes.GetLocal() then return end
 	if GameRules.IsPaused() then 
 		Engine.ExecuteCommand("dota_pause")
+		sleep(1)
 	end
 end
 
+function sleep(n)  -- seconds
+  local t0 = clock()
+  while clock() - t0 <= n do end
+end
 
 return unpauser
