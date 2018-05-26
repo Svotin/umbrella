fd2cd70a364da74d874f5db91df5eac2c184d98d
Svotin <dogweb2023@gmail.ru>
2018-05-26 23:12:11 +0300

    MAGIC_IMMUNE&NewProcast
    Проверка, есть ли у морфлинга иммунитет на применяемое заклинание, и нужно ли нажимать перекачку.
    Также более играбельная верси прокаста, теперь он будет даваться и без эзериала+с любым уровнем адаптив страйка.

--- Heroes/morph.lua
+++ Heroes/morph.lua
@@ -1,4 +1,3 @@
-
 local morph = {}
 
 morph.optionEnable = Menu.AddOptionBool({"Hero Specific", "Morph"}, "Enable", false)
@@ -12,12 +11,12 @@ morph.ability = {}
 
 
 morph.defaultAbilities = {
-	{"npc_dota_hero_axe", "axe_berserkers_call", 300},
-	{"npc_dota_hero_tidehunter", "tidehunter_ravage", 1250},
-	{"npc_dota_hero_enigma", "enigma_black_hole", 720},
-	{"npc_dota_hero_magnataur", "magnataur_reverse_polarity", 430},
-	{"npc_dota_hero_slardar", "slardar_slithereen_crush", 355},
-	{"npc_dota_hero_centaur", "centaur_hoof_stomp", 345}
+	{"npc_dota_hero_axe", "axe_berserkers_call", 300, false},   		--false - пробивает через бкб
+	{"npc_dota_hero_tidehunter", "tidehunter_ravage", 1250, true},      --true - не пробивает бкб
+	{"npc_dota_hero_enigma", "enigma_black_hole", 720, false},
+	{"npc_dota_hero_magnataur", "magnataur_reverse_polarity", 430, false},
+	{"npc_dota_hero_slardar", "slardar_slithereen_crush", 355, true},
+	{"npc_dota_hero_centaur", "centaur_hoof_stomp", 345, true}
 
 --	{"npc_dota_hero_batrider", "batrider_flaming_lasso", 170},
 --	{"npc_dota_hero_faceless_void", "faceless_void_chronosphere", 1100},
@@ -30,19 +29,19 @@ morph.defaultAbilities = {
 --	{"npc_dota_hero_disruptor", "disruptor_static_storm", 1450}
 }
 morph.additionalAbilities = {
-	{"npc_dota_hero_axe", "axe_berserkers_call", 300},
-	{"npc_dota_hero_tidehunter", "tidehunter_ravage", 1250},
-	{"npc_dota_hero_enigma", "enigma_black_hole", 720},
-	{"npc_dota_hero_magnataur", "magnataur_reverse_polarity", 430},
-	{"npc_dota_hero_slardar", "slardar_slithereen_crush", 355},
-	{"npc_dota_hero_centaur", "centaur_hoof_stomp", 345},
+	{"npc_dota_hero_axe", "axe_berserkers_call", 300, false},
+	{"npc_dota_hero_tidehunter", "tidehunter_ravage", 1250, true},
+	{"npc_dota_hero_enigma", "enigma_black_hole", 720, false},
+	{"npc_dota_hero_magnataur", "magnataur_reverse_polarity", 430, false},
+	{"npc_dota_hero_slardar", "slardar_slithereen_crush", 355, true},
+	{"npc_dota_hero_centaur", "centaur_hoof_stomp", 345, true},
 
 	--------------------------------------------------------------------
 
-	{"npc_dota_hero_batrider", "batrider_flaming_lasso", 170},
-	{"npc_dota_hero_faceless_void", "faceless_void_chronosphere", 1100},
-	{"npc_dota_hero_legion_commander", "legion_commander_duel", 150},
-	{"npc_dota_hero_pudge", "pudge_dismember", 160}
+	{"npc_dota_hero_batrider", "batrider_flaming_lasso", 170, false},
+	{"npc_dota_hero_faceless_void", "faceless_void_chronosphere", 1100, false},
+	{"npc_dota_hero_legion_commander", "legion_commander_duel", 150, false},
+	{"npc_dota_hero_pudge", "pudge_dismember", 160, false}
 }
 
 
@@ -52,38 +51,41 @@ function morph.OnUpdate()
       		if morph.players[i] then
         		Menu.RemoveOption(morph.players[i]) 
         		morph.players[i] = nil
-      end
+     		end
+    	end return 
     end
-		return end
 	morph.myHero = Heroes.GetLocal()
 	if NPC.GetUnitName(morph.myHero) ~= "npc_dota_hero_morphling" then return end
 	local FHeroes = Heroes.GetAll()
 	if Menu.IsEnabled(morph.AutoShift) then
-	if not Entity.IsAlive(morph.myHero) or NPC.IsStunned(morph.myHero) or NPC.IsSilenced(morph.myHero) then return end
-	local shift = NPC.GetAbilityByIndex(morph.myHero, 4)
-	if Menu.IsEnabled(morph.AdditionalAbilities) then
-		morph.ability = morph.additionalAbilities
-	else 
-		morph.ability = morph.defaultAbilities
-	end
-	for _,v in pairs(FHeroes) do
-		if v and Entity.IsHero(v) and Entity.IsAlive(v) and not Entity.IsSameTeam(morph.myHero, v) and not Entity.IsDormant(v) and not NPC.IsIllusion(v) then
-			for i,k in pairs(morph.ability) do
-				if NPC.GetUnitName(v) == k[1] then
-					local p1 = NPC.GetAbility(v, k[2])
-					if p1 ~= (nil or 0) and Ability.IsInAbilityPhase(p1) then								 
-						local p2 = Ability.GetCastPoint(p1)
-						if NPC.IsEntityInRange(morph.myHero, v, k[3]) then
-							if not Ability.GetToggleState(shift) then
-								Ability.Toggle(shift)
-										return
+		if not Entity.IsAlive(morph.myHero) or NPC.IsStunned(morph.myHero) or NPC.IsSilenced(morph.myHero) then return end
+		local shift = NPC.GetAbilityByIndex(morph.myHero, 4)
+		if Menu.IsEnabled(morph.AdditionalAbilities) then
+			morph.ability = morph.additionalAbilities
+		else 
+			morph.ability = morph.defaultAbilities
+		end
+		for _,v in pairs(FHeroes) do
+			if v and Entity.IsHero(v) and Entity.IsAlive(v) and not Entity.IsSameTeam(morph.myHero, v) and not Entity.IsDormant(v) and not NPC.IsIllusion(v) then
+				for i,k in pairs(morph.ability) do
+					if NPC.GetUnitName(v) == k[1] then
+						local p1 = NPC.GetAbility(v, k[2])
+						if p1 ~= (nil or 0) and Ability.IsInAbilityPhase(p1) then								 
+							local p2 = Ability.GetCastPoint(p1)
+							if NPC.IsEntityInRange(morph.myHero, v, k[3]) then
+								if NPC.HasState(morph.myHero,Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and k[4] then 
+									return 
+								end
+								if not Ability.GetToggleState(shift) then
+									Ability.Toggle(shift)
+											return
+								end
 							end
 						end
 					end
-				end
-			end		
+				end		
+			end
 		end
-	end
 	end
 	if Menu.IsEnabled(morph.AutoKill) then
 		for i = 1, Heroes.Count() do
@@ -91,42 +93,61 @@ function morph.OnUpdate()
    			if not Entity.IsSameTeam(morph.myHero, hero) and not morph.players[Hero.GetPlayerID(hero)] and hero ~= morph.myHero then 
    				morph.players[Hero.GetPlayerID(hero)] = Menu.AddOptionBool({"Hero Specific", "Morph", "EBladeAutoKill"}, string.upper(string.sub(NPC.GetUnitName(hero), 15)), false)
    				return
-   				end
-   			end 
+   			end
+   		end 
    		local strike = NPC.GetAbilityByIndex(morph.myHero, 1)
    		local eblade = NPC.GetItem(morph.myHero, "item_ethereal_blade", true)
-   		if eblade and Ability.IsReady(strike) and Ability.IsReady(eblade) then
-   			local agility = Hero.GetAgilityTotal(morph.myHero)
-   			local strenght = Hero.GetStrengthTotal(morph.myHero)
-   			local intellect = Hero.GetIntellectTotal(morph.myHero)
-   			local strikeDmg =  getStrikeDmg(agility, strenght)
-   			local ebladeDmg = 75 + (2 * agility)
-   			local intMultiplier = ((0.069 * intellect) / 100) + 1
-   			for _,hero in pairs(FHeroes) do
-				if hero ~= nil and hero ~= 0 and NPCs.Contains(hero) and NPC.IsEntityInRange(morph.myHero, hero,Ability.GetCastRange(eblade)) and NPC.IsEntityInRange(morph.myHero, hero,Ability.GetCastRange(strike)) and not Entity.IsSameTeam(hero,morph.myHero) then
-					if Entity.IsAlive(hero) and not Entity.IsDormant(hero) and not NPC.IsIllusion(hero) and Menu.IsEnabled(morph.players[Hero.GetPlayerID(hero)]) and morph.IsHasGuard(hero)=="nil"  then
-						local localDmg = ((strikeDmg+ebladeDmg)*1.4)*intMultiplier
-						local totalDmg = morph.GetTotalDmg(hero, localDmg, morph.myHero)-2
-						-- if Entity.GetHealth(hero) <= strikeDmg*intMultiplier and Ability.GetManaCost(strike) <= NPC.GetMana(morph.myHero) then
-						-- 		Ability.CastTarget(strike, hero)
-						-- 		return
-						if Entity.GetHealth(hero) <= totalDmg and (Ability.GetManaCost(strike)+Ability.GetManaCost(eblade)) <= NPC.GetMana(morph.myHero) then
+		local agility = Hero.GetAgilityTotal(morph.myHero)
+		local strenght = Hero.GetStrengthTotal(morph.myHero)
+		local intellect = Hero.GetIntellectTotal(morph.myHero)
+		local strikeDmg =  getStrikeDmg(agility, strenght, strike)
+		local totalMana = 0
+		local castRange = 0
+		local ebladeMultiplier = 1
+		local ebladeDmg = 0
+		if strikeDmg ~= 0 then 
+			totalMana = totalMana + Ability.GetManaCost(strike)
+			castRange = Ability.GetCastRange(strike)
+		end
+		if eblade and Ability.IsReady(eblade) then
+			ebladeMultiplier = 1.4
+			ebladeDmg = 75 + (2 * agility)
+			totalMana = totalMana + Ability.GetManaCost(eblade)
+			castRange = Ability.GetCastRange(eblade)
+		end
+		if ebladeDmg == 0 and strikeDmg == 0 then return end
+		local intMultiplier = ((0.069 * intellect) / 100) + 1
+		for _,hero in pairs(FHeroes) do
+			if hero ~= nil and hero ~= 0 and NPCs.Contains(hero) and NPC.IsEntityInRange(morph.myHero, hero,castRange) and not Entity.IsSameTeam(hero,morph.myHero) then
+				if Entity.IsAlive(hero) and not Entity.IsDormant(hero) and not NPC.IsIllusion(hero) and Menu.IsEnabled(morph.players[Hero.GetPlayerID(hero)]) and morph.IsHasGuard(hero)=="nil"  then
+					local localDmg = ((strikeDmg+ebladeDmg)*ebladeMultiplier)*intMultiplier
+					local totalDmg = morph.GetTotalDmg(hero, localDmg, morph.myHero) - 2
+					if Entity.GetHealth(hero) <= totalDmg and totalMana <= NPC.GetMana(morph.myHero) then
+						if ebladeDmg > 0 then
 							Ability.CastTarget(eblade, hero)
-							Ability.CastTarget(strike, hero)
-							return
 						end
+						if strikeDmg > 0 then 
+							Ability.CastTarget(strike, hero)
+						end return
 					end
 				end
 			end
 		end
-   	end
+	end
 end
-function getStrikeDmg(agility, strenght)
-	local strikeDmg = 0
-	local damageMultiplier = 0.5
+
+function getStrikeDmg(agility, strenght, strike)
+	if not strike then return 0 end
+	if not Ability.IsReady(strike) then return 0 end
+	local strikeLevel = Ability.GetLevel(strike)
+	local damageMultiplier
+	local minMultiplier  = 0.5
+	local maxMultiplier = 0.5 + (strikeLevel*0.5)
 	local totalAttributies = agility + strenght
 	if (totalAttributies/100*75 < agility) then 
-		damageMultiplier = 2.5
+		damageMultiplier = maxMultiplier
+	else
+		damageMultiplier = minMultiplier
 	end
 	strikeDmg = 100 + agility*damageMultiplier
 	return strikeDmg
@@ -202,7 +223,7 @@ function morph.GetTotalDmg(target,dmg, myHero)--ЧЕСТНО СПИЗДИЛ
 		totalDmg = totalDmg * 1.4
 	end
 	if NPC.HasModifier(target,"modifier_item_hood_of_defiance_barrier") then 
-		totalDmg = totalDmg - 400
+		totalDmg = totalDmg - 325
 	end
 	if NPC.HasModifier(target,"modifier_item_pipe_barrier") then 
 		totalDmg = totalDmg - 400
