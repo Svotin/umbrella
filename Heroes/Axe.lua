9e94277eeb865e0e0fc4dd3fb00ced0bf9078d2e
Svotin <dogweb2023@gmail.ru>
2018-05-27 21:31:35 +0300

    Виталик спасибо
    Добавил break после каста ульта + заменил IsReady на IsCastable

--- Heroes/Axe.lua
+++ Heroes/Axe.lua
@@ -11,18 +11,19 @@ function axe.OnUpdate()
 	if NPC.GetUnitName(axe.myHero) ~= "npc_dota_hero_axe" then return end
 	if not Entity.IsAlive(axe.myHero) or NPC.IsStunned(axe.myHero) or NPC.IsSilenced(axe.myHero)  then return end
 	local ulti = NPC.GetAbility(axe.myHero, "axe_culling_blade")
-	if not Ability.IsReady(ulti) then return end
+	local mana = Ability.GetManaCost(ulti)
+	if not Ability.IsCastable(ulti, mana) then return end
 	local AllHeroes = Heroes.GetAll()
 	local lvlUlti = Ability.GetLevel(ulti)
 	local damage = 175 + (75*lvlUlti)
-	local mana = Ability.GetManaCost(ulti)
 	local customRange = Menu.GetValue(axe.customRange)
 	local castRange = Ability.GetCastRange(ulti) + customRange
 	for _,hero in pairs(AllHeroes) do
 		if hero ~= nil and hero ~= 0 and NPCs.Contains(hero) and NPC.IsEntityInRange(axe.myHero, hero,castRange) and not Entity.IsSameTeam(hero,axe.myHero) then
 			if Entity.IsAlive(hero) and not Entity.IsDormant(hero) and not NPC.IsIllusion(hero) then 
-				if Entity.GetHealth(hero) <= damage and mana <= NPC.GetMana(axe.myHero) then
+				if Entity.GetHealth(hero) + NPC.GetHealthRegen(hero) <= damage and not NPC.IsLinkensProtected(hero) then
 					Ability.CastTarget(ulti, hero)
+					break
 				end
 			end
 		end
