34902c1dc705a311bc6a7e3993c436bb8182f08e
Svotin <dogweb2023@gmail.ru>
2018-06-01 15:29:16 +0300

    comboToCursor&linkenProtection&optimisation

--- Heroes/Axe.lua
+++ Heroes/Axe.lua
@@ -1,10 +1,10 @@
 local axe = {}
 
 axe.optionEnable = Menu.AddOptionBool({"Hero Specific", "Axe"}, "Enable", false)
+axe.blinkRadius = Menu.AddOptionBool({"Hero Specific", "Axe"}, "Range of Blink", false)
 axe.optionAutoUltEnable = Menu.AddOptionBool({"Hero Specific", "Axe", "Auto Culling"}, "Enable", false)
 axe.customRange = Menu.AddOptionSlider({"Hero Specific", "Axe", "Auto Culling"}, "Range to Target", 120, 300, 120)
 axe.optionKey = Menu.AddKeyOption({"Hero Specific", "Axe"}, "Combo Key", Enum.ButtonCode.KEY_Z)
-axe.enemyInRange = Menu.AddOptionSlider({"Hero Specific", "Axe"}, "Closest to mouse range for Combo", 100, 600, 100)
 axe.optionEnableBlink = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, "Blink", false)
 axe.optionEnableCrimson = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, "Crimson Guard", false)
 axe.optionEnableHood = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, "Hood of Defiance", false)
@@ -15,32 +15,32 @@ axe.optionEnableMjolnir = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, 
 axe.optionEnableLotus = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, "Lotus Orb", false)
 axe.optionEnableShiva = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, "Shiva's Guard", false)
 
+AllHeroes = nil
 axe.myHero = nil
-enemy = nil
+enemy1 = nil
 Font = Renderer.LoadFont("Tahoma", 22, Enum.FontWeight.BOLD)
+flagForCall = false  --костыль
 
 function axe.OnUpdate()
+
 	if not Menu.IsEnabled(axe.optionEnable) or not Engine.IsInGame() or not Heroes.GetLocal() then return end
 	axe.myHero = Heroes.GetLocal()
 	if NPC.GetUnitName(axe.myHero) ~= "npc_dota_hero_axe" then return end
 	if not Entity.IsAlive(axe.myHero) or NPC.IsStunned(axe.myHero) or NPC.IsSilenced(axe.myHero)  then return end
-	if Menu.IsKeyDown(axe.optionKey) then
-		if enemy == nil then 
-			enemy = Input.GetNearestHeroToCursor(Entity.GetTeamNum(axe.myHero), Enum.TeamType.TEAM_ENEMY)
-			if not NPC.IsPositionInRange(enemy, Input.GetWorldCursorPos(), Menu.GetValue(axe.enemyInRange), 0) then
-				enemy = nil 
-			end
-		else
-			if axe.Combo(axe.myHero, enemy) == false then
-				enemy = nil
-			end
-		end
+	if Menu.IsKeyDown(axe.optionKey) then 
+		enemy1 = Input.GetWorldCursorPos()
+		axe.Combo(axe.myHero, enemy1)
+	end
+	if  Menu.IsEnabled(axe.blinkRadius) then
+		Engine.ExecuteCommand("dota_range_display " .. 1200)
+	else
+		Engine.ExecuteCommand("dota_range_display " .. 0)
 	end
 	if not Menu.IsEnabled(axe.optionAutoUltEnable) then return end
 	local ulti = NPC.GetAbility(axe.myHero, "axe_culling_blade")
 	local mana = Ability.GetManaCost(ulti)
 	if not Ability.IsReady(ulti) then return end
-	local AllHeroes = Heroes.GetAll()
+	AllHeroes = Heroes.GetAll()
 	local lvlUlti = Ability.GetLevel(ulti)
 	local damage = 175 + (75*lvlUlti)
 	local customRange = Menu.GetValue(axe.customRange)
@@ -48,7 +48,7 @@ function axe.OnUpdate()
 	for _,hero in pairs(AllHeroes) do
 		if hero ~= nil and hero ~= 0 and NPCs.Contains(hero) and NPC.IsEntityInRange(axe.myHero, hero,castRange) and not Entity.IsSameTeam(hero,axe.myHero) then
 			if Entity.IsAlive(hero) and not Entity.IsDormant(hero) and not NPC.IsIllusion(hero) then 
-				if Entity.GetHealth(hero) + NPC.GetHealthRegen(hero) <= damage and not NPC.IsLinkensProtected(hero) and mana <= NPC.GetMana(axe.myHero) then
+				if Entity.GetHealth(hero) + NPC.GetHealthRegen(hero) <= damage and not axe.checkProtection(hero) and mana <= NPC.GetMana(axe.myHero) then
 					Ability.CastTarget(ulti, hero)
 					break
 				end
@@ -59,8 +59,7 @@ end
 
 
 function axe.Combo(myHero,enemy)
-	if not enemy then return false end
-	if not NPC.IsEntityInRange(myHero, enemy, 1199) then return false end
+	if not enemy then return end
     local call = NPC.GetAbility(myHero, "axe_berserkers_call")
     local Blademail = NPC.GetItem(myHero, "item_blade_mail", true)
     local blink = NPC.GetItem(myHero, "item_blink", true)
@@ -71,97 +70,73 @@ function axe.Combo(myHero,enemy)
     local pipe = NPC.GetItem(myHero, "item_pipe", true)
     local crimson = NPC.GetItem(myHero, "item_crimson_guard", true)
     local shiva = NPC.GetItem(myHero, "item_shivas_guard")
-    local callManaCost = 0
- --    local callRange = 300
-	-- if NPC.HasAbility(myHero, "special_bonus_unique_axe_2") then
-	-- 	if Ability.GetLevel(NPC.GetAbility(myHero, "special_bonus_unique_axe_2")) > 0 then
-	-- 		callRange = 400
-	-- 	end
-	-- end
+    local axePos = Entity.GetAbsOrigin(myHero)
+    local blinkPos = Vector(enemy:GetX() - axePos:GetX(),enemy:GetY() - axePos:GetY(),enemy:GetZ() - axePos:GetZ())
+    local range = (blinkPos:GetX()^2+blinkPos:GetY()^2+blinkPos:GetZ()^2)^(0.5)
+	if range > 1199 then return end
     -- if not NPC.IsEntityInRange(myHero, enemy, callRange) then
-    if Ability.IsReady(call) then 
-    	callManaCost = Ability.GetManaCost(call)
-	end
-    local myMana = NPC.GetMana(myHero) - callManaCost
+    local myMana = NPC.GetMana(myHero)
 
     if Blademail and Menu.IsEnabled(axe.optionEnableBlademail) and Ability.IsCastable(Blademail, myMana) then
       	Ability.CastNoTarget(Blademail)
-      	return true
+      --	return true
     end
     if bkb and Menu.IsEnabled(axe.optionEnableBkb) and Ability.IsCastable(bkb, myMana) then
       	Ability.CastNoTarget(bkb)
-      	return true
+      --	return true
     end
     if lotus and Menu.IsEnabled(axe.optionEnableLotus) and Ability.IsCastable(lotus, myMana) then
       	Ability.CastTarget(lotus, myHero)
-      	return true
+      --	return true
     end
     if mjolnir and Menu.IsEnabled(axe.optionEnableMjolnir) and Ability.IsCastable(mjolnir, myMana) then
       	Ability.CastTarget(mjolnir, myHero)
-      	return true
+      --	return true
     end
     if pipe and Menu.IsEnabled(axe.optionEnablePipe) and Ability.IsCastable(pipe, myMana) then
       	Ability.CastNoTarget(pipe)
-      	return true
+      --	return true
     end
     if crimson and Menu.IsEnabled(axe.optionEnableCrimson) and Ability.IsCastable(crimson, myMana) then
       	Ability.CastNoTarget(crimson)
-      	return true
+      	--return true
     end
     if hood and Menu.IsEnabled(axe.optionEnableHood) and Ability.IsCastable(hood, myMana) then
       	Ability.CastNoTarget(hood)
-      	return true
+      --	return true
     end
     if shiva and Menu.IsEnabled(axe.optionEnableShiva) and Ability.IsCastable(shiva, myMana) then
       	Ability.CastNoTarget(shiva)
-      	return true
+      --	return true
     end
-    if blink and Menu.IsEnabled(axe.optionEnableBlink) and Ability.IsReady(blink) then
-      	Ability.CastPosition(blink, Entity.GetAbsOrigin(enemy))
-      	if Ability.IsReady(call) then 
-      		Ability.CastNoTarget(call)
-      	end
-     	return false
+
+    if blink and Menu.IsEnabled(axe.optionEnableBlink) and Ability.IsReady(blink) and Ability.IsReady(call) and Ability.IsCastable(call, myMana) then
+      	Ability.CastPosition(blink,enemy)
+      	enemy1 = nil
+      	flagForCall = true
+     --	return true
     end
-    return false
+    if flagForCall then
+		Ability.CastNoTarget(call)
+		flagForCall = false
+		enemy1 = nil
+   		--return false
+   	end
+   	enemy1 = nil
+   	flagForCall = false
+    return 
 end
 
-function axe.OnDraw()
-	if axe.myHero == nil or NPC.GetUnitName(axe.myHero) ~= "npc_dota_hero_axe" or not Menu.IsEnabled(axe.optionEnable) then return end
-	local target = Input.GetNearestHeroToCursor(Entity.GetTeamNum(axe.myHero), Enum.TeamType.TEAM_ENEMY)
-	if target then
-		if not NPC.IsPositionInRange(target, Input.GetWorldCursorPos(), Menu.GetValue(axe.enemyInRange), 0) then
-			target = nil 
-		end
+function axe.checkProtection(enemy)
+	if NPC.IsLinkensProtected(enemy) then return true end
+	local spell_shield = NPC.GetAbility(enemy, "antimage_spell_shield")
+	if spell_shield and Ability.IsReady(spell_shield) and (NPC.HasModifier(enemy, "modifier_item_ultimate_scepter") or NPC.HasModifier(enemy, "modifier_item_ultimate_scepter_consumed")) then
+		return true
 	end
-	local x, y = Renderer.GetScreenSize()
-	if x == 1920 and y == 1080 then
-		x, y = 1150+205, 910+150
-	-- elseif x== 1600 and y == 900 then
-	-- 	x, y = 950, 755
-	-- elseif x== 1366 and y == 768 then
-	-- 	x, y = 805, 643
-	-- elseif x==1280 and y == 720 then
-	-- 	x, y = 752, 600
-	-- elseif x==1280 and y == 1024 then
-	-- 	x, y = 800, 860
-	-- elseif x==1440 and y == 900 then
-	-- 	x, y = 870, 755
-	-- elseif x== 1680 and y == 1050 then
-	-- 	x, y = 1025, 885
-	-- end
-	-- x = x + 210
-	-- y = y + 150
-	end
-	Renderer.DrawFilledRect(x-15, y-5, 350, 25)
-	local msg
-	Renderer.SetDrawColor(0, 0, 0)
-	if target == nil or target == 0 then 
-		msg = "NONE"
-	else 
-		msg = NPC.GetUnitName(target)
-	end
-	Renderer.DrawText(Font, x, y, "["..msg.."]")
+	if NPC.GetAbility(enemy,"special_bonus_unique_queen_of_pain") then return true end
+	if NPC.HasModifier(enemy,"modifier_dark_willow_shadow_realm_buff") then return true end
+	return false
 end
+
 
 return axe
