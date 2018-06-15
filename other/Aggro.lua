0b727036b2c418463f4ebba5badaebaf38f698cd
Svotin <dogweb2023@gmail.ru>
2018-06-15 13:40:08 +0300

    aggro with stop attack

--- other/Aggro.lua
+++ other/Aggro.lua
@@ -12,14 +12,22 @@ function Aggro.OnUpdate()
 		enemyHero = Input.GetNearestHeroToCursor(Entity.GetTeamNum(Aggro.myhero), Enum.TeamType.TEAM_ENEMY)
 		if enemyHero then
 			Player.AttackTarget(Players.GetLocal(), Aggro.myhero, enemyHero)
+			Player.PrepareUnitOrders(Players.GetLocal(), 10, nil, Vector(), nil, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, nil)
 			return
 		end
 	end
 	if Menu.IsKeyDownOnce(Aggro.deaggroKey) then
-		alliedCreep = Input.GetNearestUnitToCursor(Entity.GetTeamNum(Aggro.myhero), Enum.TeamType.TEAM_FRIEND)
-		if alliedCreep then
-			Player.AttackTarget(Players.GetLocal(), Aggro.myhero, alliedCreep)
-			return			
+		local npcs = NPCs.GetAll()
+		for _,npc in pairs(npcs) do
+			if Entity.IsSameTeam(npc, Aggro.myhero) and (NPC.IsHero(npc) or NPC.IsLaneCreep(npc)) and Entity.IsAlive(npc) and not NPC.IsWaitingToSpawn(npc) and npc~=Aggro.myhero then
+				if NPC.IsLaneCreep(npc) and Entity.GetMaxHealth(npc)/Entity.GetHealth(npc)>=2 then return end
+ 				local alliedCreep = npc
+				if alliedCreep then
+					Player.AttackTarget(Players.GetLocal(), Aggro.myhero, alliedCreep)
+					Player.PrepareUnitOrders(Players.GetLocal(), 10, nil, Vector(), nil, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_CURRENT_UNIT_ONLY, nil)
+					return			
+				end
+			end
 		end
 	end
 end
