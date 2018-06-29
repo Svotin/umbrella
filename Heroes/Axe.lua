79356272f13fc5865393e82f1c8486a3cec0a19f
Svotin <dogweb2023@gmail.ru>
2018-06-29 22:40:07 +0300

    change meepo menu place and update axe

--- Heroes/Axe.lua
+++ Heroes/Axe.lua
@@ -6,7 +6,7 @@ axe.optionAutoUltEnable = Menu.AddOptionBool({"Hero Specific", "Axe", "Auto Cull
 axe.customRange = Menu.AddOptionSlider({"Hero Specific", "Axe", "Auto Culling"}, "Range to Target", 120, 300, 120)
 axe.cullingRange = Menu.AddOptionBool({"Hero Specific", "Axe", "Auto Culling"}, "[debug]Show Auto Culling Range", false)
 axe.optionKey = Menu.AddKeyOption({"Hero Specific", "Axe"}, "Combo Key", Enum.ButtonCode.KEY_Z)
-axe.blinkType = Menu.AddOptionCombo({"Hero Specific", "Axe"}, "Blink Type", {"Blink to the best Position", "Blink to Cursor"}, 0)
+axe.blinkType = Menu.AddOptionCombo({"Hero Specific", "Axe"}, "Blink Type", {"Blink to best Position", "Blink to Cursor"}, 0)
 axe.comboType = Menu.AddOptionCombo({"Hero Specific", "Axe"}, "Combo Type", {"Blink+Call first", "Items first"}, 1)
 axe.optionEnableBlink = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, "Blink", false)
 axe.hunger = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, "Battle Hanger", false)
@@ -19,10 +19,10 @@ axe.optionEnableMjolnir = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, 
 axe.optionEnableLotus = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, "Lotus Orb", false)
 axe.optionEnableShiva = Menu.AddOptionBool({"Hero Specific", "Axe", "Combo"}, "Shiva's Guard", false)
 
-AllHeroes = nil
+local AllHeroes = nil
 axe.myHero = nil
-enemy1 = nil
-flagForCall = false  --костыль
+local enemy1 = nil
+local flagForCall = false  --костыль
 axe.sleepers = {}
 
 function axe.OnUpdate()
@@ -39,11 +39,15 @@ function axe.OnUpdate()
 		    enemy1 = Input.GetWorldCursorPos()
       end
     end
-		if axe.Combo(axe.myHero, enemy1) == false then
+		if axe.Combo(axe.myHero, enemy1) then
       enemy1 = nil
     end
 	else
     enemy1 = nil
+  end
+  if axe.SleepCheck(0.5, "items") then
+    axe.GetItems(axe.myHero)
+    axe.Sleep(0.5 , "items")
   end
   ------------------------------------------------------------------
   local ulti = NPC.GetAbility(axe.myHero, "axe_culling_blade")
@@ -60,18 +64,27 @@ function axe.OnUpdate()
 	if not Menu.IsEnabled(axe.optionAutoUltEnable) then return end
 	local mana = Ability.GetManaCost(ulti)
 	if not Ability.IsReady(ulti) then return end
-	AllHeroes = Heroes.GetAll()
+	AllHeroes = Entity.GetHeroesInRadius(axe.myHero, castRange, Enum.TeamType.TEAM_ENEMY)
+  if not AllHeroes or #AllHeroes < 1 then return end 
 	local lvlUlti = Ability.GetLevel(ulti)
 	local damage = 175 + (75*lvlUlti)
-	for _,hero in pairs(AllHeroes) do
-		if hero ~= nil and hero ~= 0 and Heroes.Contains(hero) and NPC.IsEntityInRange(axe.myHero, hero,castRange) and not Entity.IsSameTeam(hero,axe.myHero) then
-			if Entity.IsAlive(hero) and not Entity.IsDormant(hero) and not NPC.IsIllusion(hero) then 
-				if Entity.GetHealth(hero) + NPC.GetHealthRegen(hero) <= damage and not axe.checkProtection(hero) and mana <= NPC.GetMana(axe.myHero) and axe.SleepCheck(0.3, "delay") then
-					Ability.CastTarget(ulti, hero)
-          if NPC.HasModifier(hero,"modifier_skeleton_king_reincarnation_scepter") then
-            axe.Sleep(0.3,"delay")
+	for i = 1, #AllHeroes do
+		if AllHeroes[i] ~= nil and AllHeroes[i] ~= 0 then
+			if Entity.IsAlive(AllHeroes[i]) and not Entity.IsDormant(AllHeroes[i]) then 
+				if Entity.GetHealth(AllHeroes[i]) + NPC.GetHealthRegen(AllHeroes[i]) <= damage and mana <= NPC.GetMana(axe.myHero) and axe.SleepCheck(0.3, "delay") and axe.checkProtection(AllHeroes[i]) ~= "IMMUNE" then
+          if axe.checkProtection(AllHeroes[i]) == "LINKEN" then
+            local hunger_pop = NPC.GetAbilityByIndex(myHero, 1)
+            if hunger_pop and Ability.IsCastable(hunger_pop, NPC.GetMana(axe.myHero)-mana) and not NPC.HasState(AllHeroes[i], Enum.ModifierState.MODIFIER_STATE_MAGIC_IMMUNE) and not axe.IsInAbilityPhase(axe.myHero) then
+              Ability.CastTarget(hunger_pop, AllHeroes[i])
+              break
+            end
+          else
+  					Ability.CastTarget(ulti, AllHeroes[i])
+            if NPC.HasModifier(AllHeroes[i],"modifier_skeleton_king_reincarnation_scepter") then
+              axe.Sleep(0.3,"delay")
+            end
+  					break
           end
-					break
 				end
 			end
 		end
@@ -79,19 +92,22 @@ function axe.OnUpdate()
 end
 
 
+function axe.GetItems(myHero)
+  hunger = NPC.GetAbilityByIndex(myHero, 1)
+  call = NPC.GetAbility(myHero, "axe_berserkers_call")
+  Blademail = NPC.GetItem(myHero, "item_blade_mail", true)
+  blink = NPC.GetItem(myHero, "item_blink", true)
+  bkb = NPC.GetItem(myHero, "item_black_king_bar", true)
+  mjolnir = NPC.GetItem(myHero, "item_mjollnir", true)
+  lotus = NPC.GetItem(myHero, "item_lotus_orb", true)
+  hood = NPC.GetItem(myHero, "item_hood_of_defiance", true)
+  pipe = NPC.GetItem(myHero, "item_pipe", true)
+  crimson = NPC.GetItem(myHero, "item_crimson_guard", true)
+  shiva = NPC.GetItem(myHero, "item_shivas_guard")
+end
+
 function axe.Combo(myHero,enemy)
 	if not enemy then return end
-  local hunger = NPC.GetAbilityByIndex(myHero, 1)
-  local call = NPC.GetAbility(myHero, "axe_berserkers_call")
-  local Blademail = NPC.GetItem(myHero, "item_blade_mail", true)
-  local blink = NPC.GetItem(myHero, "item_blink", true)
-  local bkb = NPC.GetItem(myHero, "item_black_king_bar", true)
-  local mjolnir = NPC.GetItem(myHero, "item_mjollnir", true)
-  local lotus = NPC.GetItem(myHero, "item_lotus_orb", true)
-  local hood = NPC.GetItem(myHero, "item_hood_of_defiance", true)
-  local pipe = NPC.GetItem(myHero, "item_pipe", true)
-  local crimson = NPC.GetItem(myHero, "item_crimson_guard", true)
-  local shiva = NPC.GetItem(myHero, "item_shivas_guard")
   local axePos = Entity.GetAbsOrigin(myHero)
       ---- BLINK TO THE BEST POS ----
   if Menu.GetValue(axe.blinkType) == 0 then
@@ -159,11 +175,11 @@ function axe.Combo(myHero,enemy)
   end
   if crimson and Menu.IsEnabled(axe.optionEnableCrimson) and Ability.IsCastable(crimson, myMana) then
     	Ability.CastNoTarget(crimson)
-    	return true
+   -- 	return true
   end
   if hood and Menu.IsEnabled(axe.optionEnableHood) and Ability.IsCastable(hood, myMana) then
     	Ability.CastNoTarget(hood)
-    	return true
+   -- 	return true
   end
   if shiva and Menu.IsEnabled(axe.optionEnableShiva) and Ability.IsCastable(shiva, myMana) then
     	Ability.CastNoTarget(shiva)
@@ -190,20 +206,24 @@ function axe.Combo(myHero,enemy)
     enemy1 = nil
    return false
   end
+  if axe.SleepCheck(0.5, "attack") then
+    Player.AttackTarget(Players.GetLocal(), axe.myHero, nearHero, false)
+    axe.Sleep(0, "attack")
+  end
  	enemy1 = nil
  	flagForCall = false
   return false
 end
 
 function axe.checkProtection(enemy)
-	if NPC.IsLinkensProtected(enemy) then return true end
+	if NPC.IsLinkensProtected(enemy) then return "LINKEN" end
 	local spell_shield = NPC.GetAbility(enemy, "antimage_spell_shield")
 	if spell_shield and Ability.IsReady(spell_shield) and (NPC.HasModifier(enemy, "modifier_item_ultimate_scepter") or NPC.HasModifier(enemy, "modifier_item_ultimate_scepter_consumed")) then
-		return true
+		return "LINKEN"
 	end
-	if NPC.GetAbility(enemy,"special_bonus_unique_queen_of_pain") then return true end
-	if NPC.HasModifier(enemy,"modifier_dark_willow_shadow_realm_buff") then return true end
-  if NPC.HasModifier(enemy,"modifier_skeleton_king_reincarnation_scepter_active") then return true end
+	if NPC.GetAbility(enemy,"special_bonus_unique_queen_of_pain") then return "IMMUNE" end
+	if NPC.HasModifier(enemy,"modifier_dark_willow_shadow_realm_buff") then return "IMMUNE" end
+  if NPC.HasModifier(enemy,"modifier_skeleton_king_reincarnation_scepter_active") then return "IMMUNE" end
 
 	return false
 end
